ea3411ab1165e61e2f11c684fea5d4ecf512056a
srikanthbandi <sbandi1@kent.edu>
2017-04-18 14:51:15 -0400

    photo slide show application
    This is a Visual basic desktop applicaiton used to see photos in a
    background with navigation option and as well a automated slideshow
    feature.

---
+++ photo slideshow application/memories/MainForm.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.previousButton = New System.Windows.Forms.Button()
+        Me.nextButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.beginButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.slideshowCheckbox = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.stopslideshowButton = New System.Windows.Forms.Button()
+        Me.Album = New System.Windows.Forms.ImageList(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'previousButton
+        '
+        Me.previousButton.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.previousButton.BackColor = System.Drawing.Color.MediumBlue
+        Me.previousButton.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.previousButton.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.previousButton.Location = New System.Drawing.Point(164, 302)
+        Me.previousButton.Name = "previousButton"
+        Me.previousButton.Size = New System.Drawing.Size(75, 29)
+        Me.previousButton.TabIndex = 1
+        Me.previousButton.Text = "<< - Previous"
+        Me.previousButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage
+        Me.previousButton.UseVisualStyleBackColor = False
+        '
+        'nextButton
+        '
+        Me.nextButton.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.nextButton.Location = New System.Drawing.Point(358, 302)
+        Me.nextButton.Name = "nextButton"
+        Me.nextButton.Size = New System.Drawing.Size(75, 29)
+        Me.nextButton.TabIndex = 2
+        Me.nextButton.Text = "Next ->>"
+        Me.nextButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(499, 374)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(75, 23)
+        Me.exitButton.TabIndex = 4
+        Me.exitButton.Text = "Exit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'beginButton
+        '
+        Me.beginButton.Location = New System.Drawing.Point(261, 302)
+        Me.beginButton.Name = "beginButton"
+        Me.beginButton.Size = New System.Drawing.Size(75, 29)
+        Me.beginButton.TabIndex = 5
+        Me.beginButton.Text = "Begin"
+        Me.beginButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.OldLace
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(151, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Padding = New System.Windows.Forms.Padding(5)
+        Me.PictureBox1.Size = New System.Drawing.Size(289, 284)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'slideshowCheckbox
+        '
+        Me.slideshowCheckbox.Location = New System.Drawing.Point(164, 360)
+        Me.slideshowCheckbox.Name = "slideshowCheckbox"
+        Me.slideshowCheckbox.Size = New System.Drawing.Size(99, 24)
+        Me.slideshowCheckbox.TabIndex = 7
+        Me.slideshowCheckbox.Text = "StartSlideShow"
+        Me.slideshowCheckbox.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2000
+        '
+        'stopslideshowButton
+        '
+        Me.stopslideshowButton.Location = New System.Drawing.Point(334, 360)
+        Me.stopslideshowButton.Name = "stopslideshowButton"
+        Me.stopslideshowButton.Size = New System.Drawing.Size(99, 24)
+        Me.stopslideshowButton.TabIndex = 8
+        Me.stopslideshowButton.Text = "Stop Slideshow"
+        Me.stopslideshowButton.UseVisualStyleBackColor = True
+        '
+        'Album
+        '
+        Me.Album.ImageStream = CType(resources.GetObject("Album.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.Album.TransparentColor = System.Drawing.Color.Transparent
+        Me.Album.Images.SetKeyName(0, "39551686.png")
+        Me.Album.Images.SetKeyName(1, "43698590.png")
+        Me.Album.Images.SetKeyName(2, "66148098.png")
+        Me.Album.Images.SetKeyName(3, "119302279.png")
+        Me.Album.Images.SetKeyName(4, "150915968.png")
+        Me.Album.Images.SetKeyName(5, "183915336.png")
+        Me.Album.Images.SetKeyName(6, "196201707.png")
+        Me.Album.Images.SetKeyName(7, "198809993.png")
+        Me.Album.Images.SetKeyName(8, "439438632.png")
+        Me.Album.Images.SetKeyName(9, "605797315.png")
+        Me.Album.Images.SetKeyName(10, "613343026.png")
+        Me.Album.Images.SetKeyName(11, "614730214.jpg")
+        Me.Album.Images.SetKeyName(12, "781557356.png")
+        Me.Album.Images.SetKeyName(13, "828042703.png")
+        Me.Album.Images.SetKeyName(14, "862149855.png")
+        Me.Album.Images.SetKeyName(15, "867419686.png")
+        Me.Album.Images.SetKeyName(16, "911037597.png")
+        Me.Album.Images.SetKeyName(17, "926276741.png")
+        Me.Album.Images.SetKeyName(18, "952790312.png")
+        Me.Album.Images.SetKeyName(19, "961720139.png")
+        Me.Album.Images.SetKeyName(20, "991944330.png")
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(586, 409)
+        Me.Controls.Add(Me.stopslideshowButton)
+        Me.Controls.Add(Me.slideshowCheckbox)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.beginButton)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.nextButton)
+        Me.Controls.Add(Me.previousButton)
+        Me.ForeColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.Name = "MainForm"
+        Me.Text = "Memories"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents previousButton As System.Windows.Forms.Button
+    Friend WithEvents nextButton As System.Windows.Forms.Button
+    Friend WithEvents exitButton As System.Windows.Forms.Button
+    Friend WithEvents beginButton As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents slideshowCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents stopslideshowButton As System.Windows.Forms.Button
+    Friend WithEvents Album As ImageList
+End Class
---
+++ photo slideshow application/memories/MainForm.vb
@@ -0,0 +1,66 @@
+﻿
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public Class MainForm
+
+    Private number_photo As Integer
+
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DisplayImage()
+
+        PictureBox1.Image = Album.Images(number_photo)
+    End Sub
+
+    Private Sub beginButton_Click(sender As Object, e As EventArgs) Handles beginButton.Click
+        ' PictureBox1.Image = Album.Images(0)
+        number_photo = 0
+        DisplayImage()
+    End Sub
+
+    Private Sub previousButton_Click(sender As Object, e As EventArgs) Handles previousButton.Click
+        number_photo = number_photo - 1
+        If number_photo < 0 Then
+            number_photo = Album.Images.Count - 1
+        End If
+        DisplayImage()
+
+    End Sub
+
+    Private Sub nextButton_Click(sender As Object, e As EventArgs) Handles nextButton.Click
+        number_photo = number_photo + 1
+        If number_photo = Album.Images.Count Then
+            number_photo = 0     
+        End If
+        DisplayImage()
+
+    End Sub
+
+
+    Private Sub slideshowCheckbox_CheckedChanged(sender As Object, e As EventArgs) Handles slideshowCheckbox.CheckedChanged
+        If slideshowCheckbox.Checked = True Then
+            Timer1.Enabled = True
+        Else
+            Timer1.Enabled = False
+        End If
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Call nextButton_Click(sender, e)
+    End Sub
+
+    Private Sub stopslideshowButton_Click(sender As Object, e As EventArgs) Handles stopslideshowButton.Click
+        slideshowCheckbox.Checked = False
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
